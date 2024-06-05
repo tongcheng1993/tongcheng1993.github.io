ce6b79b93997cd9629cb156632ca49bc974b1f6b
tongcheng <1637706385@qq.com>
2024-06-05 11:57:18 +0800

    20240605115700

--- blog/src/router/index.js
+++ blog/src/router/index.js
@@ -1,37 +1,34 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
 
 const routes = [
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     {
         path: '/',
-        component: () => import(/* webpackChunkName: "contain" */ '../views/layout/contain.vue'),
+        component: () => import('../views/layout/contain.vue'),
 
         children: [
             {
                 path: "dash",
-                component: () => import(/* webpackChunkName: "dash" */ '../views/dash/dash.vue'),
+                component: () => import('../views/dash/dash.vue'),
             },
             {
                 path: 'study/college_english',
-                component: () => import(/* webpackChunkName: "study/college_english" */ '../views/study/college_english.vue'),
+                component: () => import('../views/study/college_english.vue'),
             },
             {
                 path: 'my_tool/date_tool',
-                component: () => import(/* webpackChunkName: "my_tool/date_tool" */ '../views/my_tool/date_tool.vue'),
+                component: () => import('../views/my_tool/date_tool.vue'),
             },
             {
                 path: 'it/first/chengxuyuan',
-                component: () => import(/* webpackChunkName: "it/first/chengxuyuan" */ '../views/it/first/chengxuyuan.vue'),
+                component: () => import('../views/it/first/chengxuyuan.vue'),
             },
             {
                 path: 'it/second/ruanjianshejishi',
-                component: () => import(/* webpackChunkName: "it/second/ruanjianshejishi" */ '../views/it/second/ruanjianshejishi.vue'),
+                component: () => import('../views/it/second/ruanjianshejishi.vue'),
             },
             {
                 path: 'it/third/xitongjiagoushi',
-                component: () => import(/* webpackChunkName: "it/third/xitongjiagoushi" */ '../views/it/third/xitongjiagoushi.vue'),
+                component: () => import( '../views/it/third/xitongjiagoushi.vue'),
             },
         ]
     },
