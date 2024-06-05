6f0eda506b36a03918addcb6227a43423fb2b2e0
tongcheng <1637706385@qq.com>
2024-06-05 21:19:24 +0800

    20240605211900

---
+++ vue2/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png">
+
+    </div>
+</template>
+
+<script>
+
+
+    export default {
+        name: 'Home',
+        components: {}
+    }
+</script>
+<style>
+
+
+</style>
