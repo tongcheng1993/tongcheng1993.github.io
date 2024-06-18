8cf83a2b488ddb1f52dc36656dffb0a7bc4c3be8
tongcheng <1637706385@qq.com>
2024-06-18 19:10:09 +0800

    20240618185700

--- docs/js/check_login.js
+++ docs/js/check_login.js
@@ -1,12 +1,18 @@
 (function () {
     let metoken = window.sessionStorage.getItem("meToken");
+    let toPage = window.sessionStorage.getItem("toPage");
     let location_href = window.location.href
     if (metoken) {
-
+        if(toPage === location_href ){
+            window.sessionStorage.removeItem("toPage")
+        }else{
+            window.location.href = toPage
+        }
     } else {
         if (location_href.includes("/login.html")) {
 
         } else {
+            window.sessionStorage.setItem("toPage",location_href)
             window.location.href = "/login.html"
         }
     }
