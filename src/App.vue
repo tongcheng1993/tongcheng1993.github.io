ca42f7742e9a536e9bb6b55765a588e8e985fbbe
tongcheng <1637706385@qq.com>
2024-12-21 23:26:52 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,37 @@
 </template>
 <script setup>
 </script>
-<style scoped>
-
+<style >
+html {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+  background: #fff;
+}
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+  background: #fff;
+}
+#app {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #000;
+}
+.view_app{
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+  background: #fff;
+}
 </style>
