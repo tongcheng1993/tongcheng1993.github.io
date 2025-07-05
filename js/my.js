50d6477178e1cbc60e347fb31f542ef2674012ac
tongcheng <1637706385@qq.com>
2025-07-05 14:44:55 +0800

    1

--- js/my.js
+++ js/my.js
@@ -1,3 +1,9 @@
-(function() {
+// 全局定义方法a（可通过window.a调用）
+function checkOnline() {
+    console.log(" 全局方法a已执行，当前时间：2025年7月5日 14:43（周六）");
+    // 其他逻辑...
+}
 
+(function() {
+    checkOnline();
 })()
